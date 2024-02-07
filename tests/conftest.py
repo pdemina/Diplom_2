2d0b26ab1b99679987101b22b10769c83113cfab
Polina <polinaantonovna@yandex.com>
2024-02-07 20:54:46 +0300

    Added changes after review

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,8 @@ import helper_user
 
 @pytest.fixture
 def user_fixture():
-    user = helper_user.create_user_with_unique_data()
-    yield user
-    helper_user.delete_user(user[0], user[1])
+    user_data = helper_user.create_user_with_unique_data()
+    yield user_data
+    helper_user.delete_user(user_data[0], user_data[1])
 
 
--- tests/user_creation_test.py
+++ tests/user_creation_test.py
@@ -29,9 +29,24 @@ class TestUserCreation:
         helper_user.delete_user(email, password)
         assert response.status_code == 403
 
-    @allure.title('Создание пользователя с пустыми обязательными полями приводит к ошибке')
-    @allure.description('Проверяем, что при пустых обязательных полях сообщение "Email, password and name are required fields"')
-    def test_user_creation(self):
-        response = helper_user.create_user('', '', '')
+    @allure.title('Создание пользователя с пустым обязательным полем Email приводит к ошибке')
+    @allure.description(
+        'Проверяем, что при пустом поле Email покажется сообщение "Email, password and name are required fields"')
+    def test_user_creation_with_empty_email(self):
+        response = helper_user.create_user('', '123456789', 'name')
+        assert 'Email, password and name are required fields' in response.text
+
+    @allure.title('Создание пользователя с пустым обязательным полем password приводит к ошибке')
+    @allure.description(
+        'Проверяем, что при пустом поле password покажется сообщение "Email, password and name are required fields"')
+    def test_user_creation_with_empty_password(self):
+        response = helper_user.create_user('pb@gmail.com', '', 'name')
+        assert 'Email, password and name are required fields' in response.text
+
+    @allure.title('Создание пользователя с пустым обязательным полем Name приводит к ошибке')
+    @allure.description(
+        'Проверяем, что при пустых обязательных полях сообщение "Email, password and name are required fields"')
+    def test_user_creation_with_empty_name(self):
+        response = helper_user.create_user('pb@gmail.com', '123456789', '')
         assert 'Email, password and name are required fields' in response.text
 
